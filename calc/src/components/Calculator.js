f2df57da94483558c836152410de67c868bf1f1b
k <klavyatur@yahoo.com>
2021-02-25 13:06:59 -0500

    slightly more functionality. switching branches

--- calc/src/components/Calculator.js
+++ calc/src/components/Calculator.js
@@ -5,20 +5,36 @@ const Calculator = () => {
   const [ display, setDisplay ] = useState(0);
   const [ num1, setNum1 ] = useState(0);
   const [ num2, setNum2 ] = useState(0);
+  const [ math, setMath ] = useState(false);
   
   function numClick(e) {
     e.preventDefault();
-    setDisplay(e.target.value)
+  
+    if(display === 0 || display === "+") {
+      setDisplay(e.target.value);
+    } else {
+      setDisplay(display.concat(e.target.value));
+    }
     return;
   }
 
+  function clear(e) {
+    e.preventDefault();
+    setDisplay(0);
+  }
+
+  function mathFunction(e) {
+    setNum1(display);
+    setDisplay(e.target.value);
+    setMath(true);
+  }
 
   return (
     <div style={{border: '1px solid black' }}>
       <div>
         {display}
         <div>
-        <button> C </button> 
+        <button onClick={clear}> C </button> 
         <button> ← </button> 
         <button> / </button> 
         </div>
@@ -38,7 +54,7 @@ const Calculator = () => {
           <button value="1" onClick={numClick}> 1 </button> 
           <button value="2" onClick={numClick}> 2 </button> 
           <button value="3" onClick={numClick}> 3 </button> 
-          <button> + </button>
+          <button value="+" onClick={mathFunction}> + </button>
         </div>
         <div>
           <button> +/- </button> 
